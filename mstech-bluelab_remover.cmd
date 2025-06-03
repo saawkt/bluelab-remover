bc50d2b3e200eab2799c585f4e6265484476440e
wasynxyxxk <202036399+saawkt@users.noreply.github.com>
2025-06-03 20:10:07 -0300

    0.2 beta

---
+++ mstech-bluelab_remover.cmd
@@ -0,0 +1,39 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: taskkill
+taskkill /f /im bluecore.exe /t 
+taskkill /f /im bmon.exe /t 
+taskkill /f /im nssm.exe /t 
+taskkill /f /im powershell.exe /t 
+taskkill /f /im BlueDashUser.exe /t 
+taskkill /f /im http2.exe /t 
+taskkill /f /im http.exe /t 
+taskkill /f /im winvnc.exe /t 
+taskkill /f /im vncviewer.exe /t 
+
+:: main
+rmdir /s /q "C:\mstech\Blues\bluemonitor\client\service\bin" >nul 2>&1
+rmdir /s /q "C:\mstech\Blues\bluemonitor\client\var" >nul 2>&1
+rmdir /s /q "C:\mstech\Blues\bluelab" >nul 2>&1
+rmdir /s /q "C:\mstech\BGINFO" >nul 2>&1
+rmdir /s /q "C:\mstech\fire" >nul 2>&1
+rmdir /s /q "C:\Program Files\BlueLab\resources" >nul 2>&1
+del /f /q C:\mstech\Blues\bluedash\BlueDashUser.exe >nul 2>&1
+del /f /q C:\mstech\Blues\bluedash\http2.exe >nul 2>&1
+del /f /q C:\mstech\Blues\bluedash\nssm.exe >nul 2>&1
+del /f /q C:\mstech\BlueChoco.ps1 >nul 2>&1
+del /f /q C:\mstech\BlueClean.ps1 >nul 2>&1
+del /f /q C:\Program Files\BlueLab\BlueLab.exe >nul 2>&1
+del /f /q C:\Program Files\BlueLab\resources\app.asar >nul 2>&1
+del /f /q C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\UVncVirtualDisplay64\UVncVirtualDisplay.dll >nul 2>&1
+del /f /q C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\winvnc.exe >nul 2>&1
+del /f /q C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\vncviewer.exe >nul 2>&1
+del /f /q C:\bluedu\blues\bluecore\bluecoreUser.exe >nul 2>&1
+del /f /q C:\bluedu\blues\bluecore\integrity.cmd >nul 2>&1
+
+powershell Invoke-WebRequest -Uri "https://github.com/saawkt/bluelab-remover/raw/refs/heads/main/disable.reg" -OutFile "C:\Windows\Temp\disable.reg"
+cd /d C:\Windows\Temp >nul 2>&1
+regedit /s Disable.reg
+exit /b
+
