28544ab732ff71df1a789b861db377676303b75f
wasynxyxxk <202036399+saawkt@users.noreply.github.com>
2025-06-24 21:10:32 -0300

    fix 2.1
    Co-Authored-By: misenplase <197342816+mlsenplase@users.noreply.github.com>

--- mstech-bluelab_disable.cmd
+++ mstech-bluelab_disable.cmd
@@ -36,34 +36,34 @@ sc stop mstech-client-v3
 sc stop mstech-serial-binder
 
 :: main
-mkdir "%userprofile%\loser"
-mkdir "%userprofile%\loser\loser2"
-mkdir "%userprofile%\loser\reg"
-move "C:\mstech\Blues\bluemonitor\client\service\bin" "%userprofile%\loser" >nul 2>&1
-move "C:\mstech\Blues\bluemonitor\client\var" "%userprofile%\loser" >nul 2>&1
-move "C:\mstech\Blues\bluelab" "%userprofile%\loser" >nul 2>&1
-move "C:\mstech\BGINFO" "%userprofile%\loser" >nul 2>&1
-move "C:\mstech\fire" "%userprofile%\loser" >nul 2>&1
-move "C:\Program Files\BlueLab\resources" "%userprofile%\loser" >nul 2>&1
-move "C:\Program Files\BlueLab\BlueLab.exe" "%userprofile%\loser" >nul 2>&1
-move "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\UVncVirtualDisplay64\UVncVirtualDisplay.dll" "%userprofile%\loser" >nul 2>&1
-move "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\winvnc.exe" "%userprofile%\loser" >nul 2>&1
-move "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\vncviewer.exe" "%userprofile%\loser" >nul 2>&1
-move "C:\bluedu\blues\bluecore\bluecoreUser.exe" "%userprofile%\loser" >nul 2>&1 
-move "C:\bluedu\blues\bluecore\integrity.cmd" "%userprofile%\loser" >nul 2>&1
+mkdir "C:\loser"
+mkdir "C:\loser\loser2"
+mkdir "C:\loser\reg"
+move "C:\mstech\Blues\bluemonitor\client\service\bin" "C:\loser" >nul 2>&1
+move "C:\mstech\Blues\bluemonitor\client\var" "C:\loser" >nul 2>&1
+move "C:\mstech\Blues\bluelab" "C:\loser" >nul 2>&1
+move "C:\mstech\BGINFO" "C:\loser" >nul 2>&1
+move "C:\mstech\fire" "C:\loser" >nul 2>&1
+move "C:\Program Files\BlueLab\resources" "C:\loser" >nul 2>&1
+move "C:\Program Files\BlueLab\BlueLab.exe" "C:\loser" >nul 2>&1
+move "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\UVncVirtualDisplay64\UVncVirtualDisplay.dll" "C:\loser" >nul 2>&1
+move "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\winvnc.exe" "C:\loser" >nul 2>&1
+move "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64\vncviewer.exe" "C:\loser" >nul 2>&1
+move "C:\bluedu\blues\bluecore\bluecoreUser.exe" "C:\loser" >nul 2>&1 
+move "C:\bluedu\blues\bluecore\integrity.cmd" "C:\loser" >nul 2>&1
 :: sc delete uvnc_service
 :: del /f /q C:\Program Files\BlueLab\resources\app.asar >nul 2>&1
 
 :: this is probably not from bluelab, but from where I got the files there was this running too 2
-move "C:\mstech\Blues\bluedash\BlueDashUser.exe" "%userprofile%\loser\loser2" >nul 2>&1
-move "C:\mstech\Blues\bluedash\http2.exe" "%userprofile%\loser\loser2" >nul 2>&1
-move "C:\mstech\Blues\bluedash\nssm.exe" "%userprofile%\loser\loser2" >nul 2>&1
-move "C:\mstech\BlueChoco.ps1" "%userprofile%\loser\loser2" >nul 2>&1
-move "C:\mstech\BlueClean.ps1" "%userprofile%\loser\loser2" >nul 2>&1
+move "C:\mstech\Blues\bluedash\BlueDashUser.exe" "C:\loser\loser2" >nul 2>&1
+move "C:\mstech\Blues\bluedash\http2.exe" "C:\loser\loser2" >nul 2>&1
+move "C:\mstech\Blues\bluedash\nssm.exe" "C:\loser\loser2" >nul 2>&1
+move "C:\mstech\BlueChoco.ps1" "C:\loser\loser2" >nul 2>&1
+move "C:\mstech\BlueClean.ps1" "C:\loser\loser2" >nul 2>&1
 
 :: reg saves
-reg export "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google" "%userprofile%\loser\reg\glg.reg" /y 
-reg export "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge" "%userprofile%\loser\reg\edge.reg" /y 
+reg export "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google" "C:\loser\reg\glg.reg" /y 
+reg export "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge" "C:\loser\reg\edge.reg" /y 
 
 start /b "" cmd /c del "%~f0"&exit /b
 
--- mstech-bluelab_enable.cmd
+++ mstech-bluelab_enable.cmd
@@ -13,32 +13,32 @@ sc start mstech-client-v3
 sc start mstech-serial-binder
 
 :: main
-move "%userprofile%\loser\bin" "C:\mstech\Blues\bluemonitor\client\service" >nul 2>&1
-move "%userprofile%\loser\var" "C:\mstech\Blues\bluemonitor\client" >nul 2>&1
-move "%userprofile%\loser\bluelab" "C:\mstech\Blues" >nul 2>&1
-move "%userprofile%\loser\BGINFO" "C:\mstech" >nul 2>&1
-move "%userprofile%\loser\fire" "C:\mstech" >nul 2>&1
-move "%userprofile%\loser\resources" "C:\Program Files\BlueLab" >nul 2>&1 
-move "%userprofile%\loser\BlueLab.exe" "C:\Program Files\BlueLab" >nul 2>&1 
-move "%userprofile%\loser\UVncVirtualDisplay64\UVncVirtualDisplay.dll" "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64" >nul 2>&1
-move "%userprofile%\loser\winvnc.exe" "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64" >nul 2>&1
-move "%userprofile%\loser\vncviewer.exe" "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64" >nul 2>&1
-move "%userprofile%\loser\bluecoreUser.exe" "C:\bluedu\blues\bluecore" >nul 2>&1
-move "%userprofile%\loser\integrity.cmd" "C:\bluedu\blues\bluecore" >nul 2>&1
+move "C:\loser\bin" "C:\mstech\Blues\bluemonitor\client\service" >nul 2>&1
+move "C:\loser\var" "C:\mstech\Blues\bluemonitor\client" >nul 2>&1
+move "C:\loser\bluelab" "C:\mstech\Blues" >nul 2>&1
+move "C:\loser\BGINFO" "C:\mstech" >nul 2>&1
+move "C:\loser\fire" "C:\mstech" >nul 2>&1
+move "C:\loser\resources" "C:\Program Files\BlueLab" >nul 2>&1 
+move "C:\loser\BlueLab.exe" "C:\Program Files\BlueLab" >nul 2>&1 
+move "C:\loser\UVncVirtualDisplay64\UVncVirtualDisplay.dll" "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64" >nul 2>&1
+move "C:\loser\winvnc.exe" "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64" >nul 2>&1
+move "C:\loser\vncviewer.exe" "C:\Program Files\BlueLab\resources\BlueLab_VNC\x64" >nul 2>&1
+move "C:\loser\bluecoreUser.exe" "C:\bluedu\blues\bluecore" >nul 2>&1
+move "C:\loser\integrity.cmd" "C:\bluedu\blues\bluecore" >nul 2>&1
 
 :: this is probably not from bluelab, but from where I got the files there was this running too 2
-move "%userprofile%\loser\loser2\BlueDashUser.exe" "C:\mstech\Blues\bluedash" >nul 2>&1
-move "%userprofile%\loser\loser2\http2.exe" "C:\mstech\Blues\bluedash" >nul 2>&1
-move "%userprofile%\loser\loser2\nssm.exe" "C:\mstech\Blues\bluedash" >nul 2>&1
-move "%userprofile%\loser\loser2\BlueChoco.ps1" "C:\mstech" >nul 2>&1
-move "%userprofile%\loser\loser2\BlueClean.ps1" "C:\mstech" >nul 2>&1
+move "C:\loser\loser2\BlueDashUser.exe" "C:\mstech\Blues\bluedash" >nul 2>&1
+move "C:\loser\loser2\http2.exe" "C:\mstech\Blues\bluedash" >nul 2>&1
+move "C:\loser\loser2\nssm.exe" "C:\mstech\Blues\bluedash" >nul 2>&1
+move "C:\loser\loser2\BlueChoco.ps1" "C:\mstech" >nul 2>&1
+move "C:\loser\loser2\BlueClean.ps1" "C:\mstech" >nul 2>&1
 
 :: reg import
-reg import "%userprofile%\loser\reg\glg.reg" 
-reg import "%userprofile%\loser\reg\edge.reg" 
+reg import "C:\loser\reg\glg.reg" 
+reg import "C:\loser\reg\edge.reg" 
 
 :: clean
-rmdir "%userprofile%\loser"
+rmdir "C:\loser"
 start /b "" cmd /c del "%~f0"&exit /b
 
 
